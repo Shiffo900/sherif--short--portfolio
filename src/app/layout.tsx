098a710f9787d540355bd3cb4f97314d97101265
Shiffo900 <161572460+Shiffo900@users.noreply.github.com>
2026-08-14 19:55:50 +0300

    Refine quick portfolio (layout.tsx)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -83,7 +83,7 @@ export default function RootLayout({
                 }}
               />
             </div>
-            <div className="relative z-10 max-w-2xl mx-auto py-12 pb-24 sm:py-24 px-6">
+            <div className="relative z-10 max-w-2xl mx-auto py-12 pb-32 sm:py-24 sm:pb-36 px-6">
               {children}
             </div>
             <Navbar />
