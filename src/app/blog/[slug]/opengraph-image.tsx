e68de755a08081380da24930d01afcf8e1e3e87e
Shiffo900 <161572460+Shiffo900@users.noreply.github.com>
2026-08-15 14:52:48 +0300

    Correct Open Graph static export syntax

--- src/app/blog/[slug]/opengraph-image.tsx
+++ src/app/blog/[slug]/opengraph-image.tsx
@@ -4,7 +4,8 @@ import { ImageResponse } from "next/og";
 import { allPosts } from "content-collections";
 import { DATA } from "@/data/resume";
 
-export const runtime = "edge";\nexport const dynamic = "force-static";
+export const runtime = "edge";
+export const dynamic = "force-static";
 
 export const alt = "Blog Post";
 export const size = {
